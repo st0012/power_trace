03bb4048a07ddb27e40f5f6cc661ca29641e77d2
st0012 <stan001212@gmail.com>
2020-07-09 13:42:25 +0800

    Refactor Stack class

--- lib/power_trace/stack.rb
+++ lib/power_trace/stack.rb
@@ -5,20 +5,12 @@ module PowerTrace
   class Stack
     include Enumerable
 
+    attr_reader :entries
+
     def initialize(options = {})
-      frame_manager = PryStackExplorer.frame_manager(Pry.new)
-      frames = frame_manager.bindings
-      power_trace_index = frames.index { |b| b.frame_description&.to_sym == :power_trace }
       @output_options = extract_output_options(options)
       @options = options
-      @entries = frames[power_trace_index+2..].map do |b|
-        case b.frame_type
-        when :method
-          MethodEntry.new(b)
-        when :block
-          BlockEntry.new(b)
-        end
-      end
+      @entries = extract_entries
     end
 
     def each(&block)
@@ -35,6 +27,23 @@ module PowerTrace
       output_options = { colorize: options.fetch(:colorize, true) }
       options.delete(:colorize)
       output_options
+    end
+
+    def frame_manager
+      PryStackExplorer.frame_manager(Pry.new)
+    end
+
+    def extract_entries
+      frames = frame_manager.bindings
+      power_trace_index = frames.index { |b| b.frame_description&.to_sym == :power_trace }
+      frames[power_trace_index+2..].map do |b|
+        case b.frame_type
+        when :method
+          MethodEntry.new(b)
+        when :block
+          BlockEntry.new(b)
+        end
+      end
     end
   end
 end
