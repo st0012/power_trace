33a73b7668d08fb43a61c1536b1625db2cae94eb
st0012 <stan001212@gmail.com>
2020-07-10 22:28:01 +0800

    Support replacing exceptions' backtrace with power_trace
    1. This feature is disabled by default. Users can enable it with
    `PowerTrace.replace_backtrace = true`.
    2. This feature also comes from `PowerTrace.colorize_backtrace` option.
    Users can use it to decide if the power_trace should be colorized or
    not. Default is `true`.

--- lib/power_trace.rb
+++ lib/power_trace.rb
@@ -2,6 +2,11 @@ require "power_trace/version"
 require "power_trace/stack"
 
 module PowerTrace
+  cattr_accessor :replace_backtrace, instance_accessor: false
+  cattr_accessor :colorize_backtrace, instance_accessor: false
+  self.replace_backtrace = false
+  self.colorize_backtrace = true
+
   def power_trace(options = {})
     PowerTrace::Stack.new(options)
   end
--- lib/power_trace/exception_patch.rb
+++ lib/power_trace/exception_patch.rb
@@ -1,14 +1,23 @@
-class Exception
+class StandardError
   attr_accessor :power_trace
 end
 
 TracePoint.trace(:raise) do |tp|
   begin
     e = tp.raised_exception
+
+    next unless e.is_a?(StandardError)
+
     e.power_trace = power_trace(exception: true)
+
+    if PowerTrace.replace_backtrace
+      next if e.is_a?(LoadError)
+      next if e.is_a?(NameError)
+      e.set_backtrace(e.power_trace.to_backtrace(colorize: PowerTrace.colorize_backtrace))
+    end
   rescue => e
-    puts e
-    puts e.backtrace
-    fail "power_trace BUG"
+    puts(e)
+    puts(e.backtrace)
+    puts("power_trace's BUG")
   end
 end
