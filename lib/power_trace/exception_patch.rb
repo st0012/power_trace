256dd3b560be4c370fbb0a29618d740a8008addc
st0012 <stan001212@gmail.com>
2020-07-12 23:55:33 +0800

    Fix error skip logic

--- lib/power_trace/exception_patch.rb
+++ lib/power_trace/exception_patch.rb
@@ -7,12 +7,18 @@ TracePoint.trace(:raise) do |tp|
     e = tp.raised_exception
 
     next unless e.is_a?(StandardError)
+    next if e.is_a?(LoadError)
+    next if e.is_a?(NameError)
+    next if e.is_a?(SystemCallError)
+
+    if defined?(Bootsnap)
+      next if e.is_a?(Bootsnap::LoadPathCache::FallbackScan)
+      next if e.is_a?(Bootsnap::LoadPathCache::ReturnFalse)
+    end
 
     e.power_trace = power_trace(exception: true)
 
     if PowerTrace.replace_backtrace
-      next if e.is_a?(LoadError)
-      next if e.is_a?(NameError)
       e.set_backtrace(e.power_trace.to_backtrace(colorize: PowerTrace.colorize_backtrace))
     end
   rescue => e
