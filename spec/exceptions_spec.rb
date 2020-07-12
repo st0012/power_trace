a543b5db6780b37b61ae5af413a8ee824ad353fd
st0012 <stan001212@gmail.com>
2020-07-12 20:03:36 +0800

    Make extra info's indentation and option (extra_info_indent)

--- spec/exceptions_spec.rb
+++ spec/exceptions_spec.rb
@@ -48,6 +48,28 @@ RSpec.describe PowerTrace do
     expect(exception.power_trace.to_s(colorize: false)).to match(expected_power_trace)
   end
 
+  context "with extra_info_indent: Int" do
+    let(:expected_power_trace) do
+/.*:\d+:in `forth_call'
+        \(Arguments\)
+        num1: 20
+        num2: 10
+.*:\d+:in `block in second_call'
+        \(Locals\)
+        ten: 10
+        num: 20
+.*:\d+:in `third_call_with_block'
+        \(Arguments\)
+        block: #<Proc:.*@.*:\d+>
+.*:\d+:in `second_call'
+        \(Arguments\)
+        num: 20/
+    end
+    it "indents the extra information according to the given value" do
+      expect(exception.power_trace.to_s(colorize: false, extra_info_indent: 8)).to match(expected_power_trace)
+    end
+  end
+
   context "when PowerTrace.replace_backtrace = true" do
     around do |example|
       begin
