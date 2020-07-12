da21b8d96f6d7211d0e370288f18a97c512f77f8
st0012 <stan001212@gmail.com>
2020-07-12 21:48:57 +0800

    Prevent MethodEntry from crashing because of NoMethodError

--- lib/power_trace/entries/method_entry.rb
+++ lib/power_trace/entries/method_entry.rb
@@ -2,12 +2,20 @@ module PowerTrace
   class MethodEntry < Entry
     def method
       @method ||= Object.instance_method(:method).bind(@receiver).call(name)
+    rescue NameError
+      # if any part of the program uses Refinement to extend its methods
+      # we might still get NoMethodError when trying to get that method outside the scope
+      nil
     end
 
     private
 
     def method_parameters
-      method.parameters.map { |parameter| parameter[1] }
+      if method
+        method.parameters.map { |parameter| parameter[1] }
+      else
+        []
+      end
     end
 
     def defined_class
