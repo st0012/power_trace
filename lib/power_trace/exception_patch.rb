a86025b53fd42f9e2e2fceccac279b81aae2f005
st0012 <stan001212@gmail.com>
2020-07-13 18:23:35 +0800

    Add comments to exception patch

--- lib/power_trace/exception_patch.rb
+++ lib/power_trace/exception_patch.rb
@@ -2,14 +2,21 @@ class StandardError
   attr_accessor :stored_power_trace
 end
 
+# the purposes of this section are:
+# 1. capture and store a power_trace when the exception is firstly raised (will skip re-raising conditions)
+# 2. if `replace_backtrace` is set to `true, it'll use the captured power_trace to replace the exception's original backtrace
 TracePoint.trace(:raise) do |tp|
   begin
     e = tp.raised_exception
 
+    # it's too risky to deal with non-StandardError exceptions
     next unless e.is_a?(StandardError)
 
+    # ignore re-raised exceptions
     next if e.stored_power_trace
 
+    # these errors are commonly used as flow control so working with them can be error-prone and slow
+    # we can revisit them once the gem gets more stable
     next if e.is_a?(LoadError)
     next if e.is_a?(NameError)
     next if e.is_a?(SystemCallError)
